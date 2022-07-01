85185becfdbdeee908c95f623a97fe096fb1f91b
happyzleaf <marco@montanari.cc>
2022-07-01 11:37:22 +0200

    Updated startup script

--- .zshrc
+++
@@ -1,4 +0,0 @@
-# Nel .zshrc vanno aggiunte:
-export ZSH="$HOME/.local/src/mdotfiles/.oh-my-zsh"
-ZSH_THEME="happyz"
-
---
+++ startup.sh
@@ -0,0 +1,9 @@
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# Oh My ZSH
+export ZSH="$SCRIPT_PATH/.oh-my-zsh"
+ZSH_THEME="happyz"
+
+# Config
+export XDG_CONFIG_HOME="$SCRIPT_PATH/.config"
+
