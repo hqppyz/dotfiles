4734bfe67d9c732b7c4710e2d676dd6b8ac8979e
happyzleaf <marco@montanari.cc>
2022-07-01 16:53:11 +0200

    Added oh my zsh plugins and startup

--- startup.sh
+++ startup.sh
@@ -1,9 +1,11 @@
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+# Config
+export XDG_CONFIG_DIRS="$SCRIPT_PATH/.config:$XDG_CONFIG_DIRS"
+
 # Oh My ZSH
 export ZSH="$SCRIPT_PATH/.oh-my-zsh"
 ZSH_THEME="happyz"
-
-# Config
-export XDG_CONFIG_DIRS="$SCRIPT_PATH/.config:$XDG_CONFIG_DIRS"
+plugins=(git)
+source $ZSH/oh-my-zsh.sh
 
